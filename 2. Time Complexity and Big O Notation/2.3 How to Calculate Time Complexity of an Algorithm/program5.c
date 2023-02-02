c91af363d4d1883f5b8956fc38cae9d8c3f99bb0
Harshal Ankush Chaudhari <95923197+Strangercodes@users.noreply.github.com>
2023-02-02 13:47:44 +0530

    2 Time complexity

---
+++ 2. Time Complexity and Big O Notation/2.3 How to Calculate Time Complexity of an Algorithm/program5.c
@@ -0,0 +1,10 @@
+// This is a partially implemented code snippet
+#include <stdio.h>
+int sum(Node node)
+{
+    if (node == NULL)
+    {
+        return 0;
+    }
+    return sum(node.left) + node.value + sum(node.right);
+}
